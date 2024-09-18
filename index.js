e6acc31b2bd19b72e82fbd5cd3e2ce2c7230176c
Macbook <macbook@Apples-MacBook-Pro.local>
2024-09-18 19:20:07 +0500

    new deploy

--- index.js
+++ index.js
@@ -13,6 +13,8 @@ app.use(cors())
 app.use(express.json())
 
 app.use('/api', route)
+// app.use(bodyParser.json()); app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: true }));
 
 
 app.get('/',(req, res)=>{
